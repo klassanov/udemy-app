36af32bea726b0c651ea9cb4e8d4fc84f503b3be
Alexander Klassanov <klassanov@live.com>
2023-09-02 22:32:26 +0300

    Servers component added and registered

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //templateUrl: './servers.component.html',
+  template: `<app-server></app-server>
+             <h4>Inline template</h4>
+            <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
