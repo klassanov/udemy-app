08b16a0c5738a72d5950908064e312abd42faea0
Alexander Klassanov <klassanov@live.com>
2023-09-04 18:24:45 +0300

    Added warning and success components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import {ServersComponent} from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,   
+    ServersComponent,
+    SuccessComponent,
+    WarningComponent   
   ],
   imports: [
     BrowserModule,
