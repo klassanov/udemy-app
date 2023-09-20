8fe446cbf7a7d9cbb6da423820335ac3f7a70886
Alexander Klassanov <klassanov@live.com>
2023-09-20 16:01:16 +0300

    Event binding example

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  // Inline declaration is also possible
   // template: `<app-server></app-server>
   //            <h4>Inline template</h4>
   //           <app-server></app-server>`,
@@ -12,6 +13,7 @@ export class ServersComponent {
 
   allowNewServer: boolean = false;
   serverCreationStatus = "No server was created";
+  updatedServerName = '';
 
   constructor(){
     setTimeout(()=>{
@@ -21,5 +23,15 @@ export class ServersComponent {
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event){
+    //event.target.value is ok if event is of type any, but we decided to specify its typa
+    console.log(event);
+
+    //Explicit cast and assing to variable
+    let currentValue = (<HTMLInputElement>event.target).value;
+    console.log(currentValue);
+    this.updatedServerName = currentValue;
   }
 }
